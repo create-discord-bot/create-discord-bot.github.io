bdf2278e52a188218b4bcdae594706a3db1cd209
FLzyy <flzyy72@gmail.com>
2022-12-22 14:01:30 +0800

    delete some stuff

--- templates/javascript/default/src/events/ready.js
+++
@@ -1,9 +0,0 @@
-import { Events } from "discord.js";
-
-export const event = {
-    name: Events.ClientReady,
-    once: true,
-    execute(client) {
-        console.log(`🟢 ${client.user.tag} is up!`);
-    }
-};
--- templates/typescript/default/src/events/ready.ts
+++
@@ -1,9 +0,0 @@
-import { Client, Events } from 'discord.js';
-
-export const event = {
-    name: Events.ClientReady,
-    once: true,
-    execute(client: Client) {
-        console.log(`🟢 ${client.user.tag} is up!`);
-    }
-};
