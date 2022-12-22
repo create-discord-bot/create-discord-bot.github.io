eea24e0bcabb78d4542ce847486a9e7a0e173a51
FLzyy <flzyy72@gmail.com>
2022-12-22 21:57:22 +0800

    fix config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'create-discord-bot',
   tagline: 'Bootstrap your discord bot with blazingly fast speeds.',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://create-discord-bot.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'FLzyy', // Usually your GitHub org/user name.
-  projectName: 'create-discord-bot', // Usually your repo name.
+  organizationName: 'create-discord-bot', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
