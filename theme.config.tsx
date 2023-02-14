4d727bee571b75d00f0a6a022ab93116521ae50b
FLzyy <flzyy72@gmail.com>
2023-02-14 19:45:48 +0800

    theme config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>create-discord-bot</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/create-discord-bot/create-discord-bot.github.io",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase:
+    "https://github.com/create-discord-bot/create-discord-bot.github.io/tree/main/pages",
   footer: {
-    text: 'Nextra Docs Template',
+    text: `Copyright © ${new Date().getFullYear()} create-discord-bot.`,
   },
-}
+};
 
-export default config
+export default config;
